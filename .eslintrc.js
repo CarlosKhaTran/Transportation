c2d7a02a56293102c53789a2c76fc804b867fdc1
khatdn-ibl <khatdn@blockchainlabs.asia>
2019-04-04 01:41:14 +0700

    init new project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'react/destructuring-assignment': 'off',
+    'import/prefer-default-export': 'off',
+    "global-require": 0
+  },
+  'globals': {
+    "fetch": false,
+    "require": false,
+    "React$Node": false,
+    "isNaN": false,
+    "FormData": false
+  }
+}
