4bd9ae5bd1ac61fcb1e4f8a761e973b3b6673c6d
Luke-tsf <qhung.bm0409@gmail.com>
2019-04-16 20:16:47 +0700

    Update API

---
+++ src/service/fakedata.js
@@ -0,0 +1,44 @@
+export const fakedBill = [
+    {
+        "rowId": 0,
+        "xdocK_Doc_Entry": 5978497,
+        "route_ID": 26,
+        "store_Code": "3825",
+        "delivery_Date": "2019-04-11T00:00:00",
+        "item_Code": "9932848000015",
+        "item_Name": "CHÂN GIÒ HEO",
+        "soBich": 2,
+        "div_Unit": "Mảng (2kg/mảng)",
+        "actual_Received": 2,
+        "notes": null,
+        "created": "0001-01-01T00:00:00",
+        "createdBy": null,
+        "modified": null,
+        "modifiedBy": null
+    },
+    {
+        "rowId": 0,
+        "xdocK_Doc_Entry": 5978892,
+        "route_ID": 26,
+        "store_Code": "3825",
+        "delivery_Date": "2019-04-11T00:00:00",
+        "item_Code": "9932848000030",
+        "item_Name": "XƯƠNG HEO CÓ THỊT",
+        "soBich": 4,
+        "div_Unit": "Bịch",
+        "actual_Received": 3,
+        "notes": null,
+        "created": "0001-01-01T00:00:00",
+        "createdBy": null,
+        "modified": null,
+        "modifiedBy": null
+    }
+]
+export const fakedRatingContent = 
+{
+    "rowId": 0,
+    "store_Code": "3825",
+    "delivery_Date": "2019-04-11T00:00:00",
+    "rating": "4",
+    "ratingNotes": "BINH NOTES"
+}
--- src/service/urls.js
+++ src/service/urls.js
@@ -1,4 +1,6 @@
 // @flow
-import { HOST } from './config';
+import { HOST,USERNAME } from './config';
 
 export const getListBillUrl = (storeID: string, date: string): string => `${HOST}/PhieuGiaoHang/getlist/${storeID}/${date}`;
+export const putInsertBillUrl = (): string => `${HOST}/PhieuGiaoHang/InsertPGH?userName=${USERNAME}`;
+export const putInsertRatingUrl = (): string => `${HOST}/Rating/Insert?userName=${USERNAME}`;
